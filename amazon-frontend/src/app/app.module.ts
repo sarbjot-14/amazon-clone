e330f0b35f1a0fe0c9e2147608a28549a6120c83
sarbjot-konectAI <sarbjot.s@konect.ai>
2023-12-29 20:24:56 -0800

    frontend navbar done

---
+++ amazon-frontend/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SharedModule } from './shared/shared.module.ts/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ amazon-frontend/src/app/shared/shared.module.ts/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [CommonModule, MatToolbarModule, MatIconModule],
+  exports: [NavbarComponent],
+})
+export class SharedModule {}
